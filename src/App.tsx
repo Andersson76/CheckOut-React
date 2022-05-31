1069fc5458dc50db858072e0001bc23de469e605
Lisa Karnerfors <lisa.karnerfors@live.com>
2022-05-31 14:53:07 +0200

    Render products

--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-/* import List from './components/list'  */
-
-export default function App() {
-/*   return (
-    <List></List>
-  ) */
-}
-
---
+++ src/data/colors.tsx
@@ -0,0 +1,12 @@
+export interface Colors {
+    primary: string,
+    secondary: string,
+    third: string
+}
+
+
+export const colors: Colors = {
+    primary: "#000000",
+    secondary: "",
+    third: " #EEEEEE"
+}
