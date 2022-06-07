88d08332659b261913f76e38eb7fbc7f10231261
Lisa Karnerfors <lisa.karnerfors@live.com>
2022-06-07 19:32:48 +0200

    Fixed router correctly

--- src/components/pages/checkout.tsx
+++ src/components/pages/checkout.tsx
@@ -2,15 +2,20 @@ import { CSSProperties, FC } from 'react'
 import { useState } from "react"
 import { productList, Product } from "../../data/productlist"
 import {Button, ButtonGroup} from '@mui/material'
+import { textStyle, fontFamily, styleBtn } from "../../css/common"
+import CheckoutCard from '../products/checkoutCard'
 
 interface Props {}
 
 const CheckOut: FC<Props> = (props) => {
 
 
+  
+
     return (
         <div>
-            <p>Kundvagn ska lista tillagda produkter. bild, title, antal, pris och kundvagnens totalpris</p>
+            <p style={{...textStyle, ...fontFamily}}>Kundvagn ska lista tillagda produkter. bild, title, antal, pris och kundvagnens totalpris</p>
+
         </div>
        
     )
