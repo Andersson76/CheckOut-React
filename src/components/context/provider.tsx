e033cbadbb6c6c43083c188ced7f01897fc3bcc0
Martin Andersson <martin.andersson@me.com>
2022-06-07 14:36:25 +0200

    Created context

--- src/components/context/provider.tsx
+++ src/components/context/provider.tsx
@@ -1,14 +1,37 @@
-
-import { useState, useEffect, FC, PropsWithChildren } from 'react'
+import React from 'react'
+import  { useState, useEffect, FC, PropsWithChildren, createContext } from 'react'
 import { Product } from '../../data/productlist'
+import Products from '../pages/product'
+import CartItems from './test'
+
+
+interface ContextData {
+    productList: Product[],
+    cartItems: Product,
+    quantity: number,
+}
+
+const CartContext = React.createContext<ContextData>({}) 
 
 
 interface Props {
     product: Product
 }
+/*
+const defaultContext: ContextData = {
+    productList: [],
+    addProductToCart: () => {},
+    removeProductFromCart: () => {}
+  }
+*/
 
-const cartLogic: FC<PropsWithChildren<Props>> = (props) => {
+export const addToCart = createContext(()=> {})
+export const removeCart = createContext(() => {})
+
+
+const CartProvide: FC<PropsWithChildren<Props>> = (props) => {
     
+    const [cartItems, setCartItems] = useState([]) // State, funktion, Hook = empty array // Contex? 
 
     return (
         <div>
@@ -18,5 +41,7 @@ const cartLogic: FC<PropsWithChildren<Props>> = (props) => {
     )
 }
 
-export default cartLogic
+export default CartProvide
+
+
     
