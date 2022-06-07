e40aaae2c25a6bbd2d03d050a0e2bd0958ce6b84
Lisa Karnerfors <lisa.karnerfors@live.com>
2022-06-07 12:10:08 +0200

    Cotext

--- src/components/cart-buttons/add.tsx
+++ src/components/cart-buttons/add.tsx
@@ -2,6 +2,14 @@ import { CSSProperties, FC } from "react"
 
 interface Props {}
 
+
+// Kopplas ihop med context? 
+
+// Logiken nedan ska ligga här för att hitta rätt Id till produkt
+
+/* const foundProduct = productList.find((product) => Number(productId) == product.id) */
+
+
 const CheckOut: FC<Props> = (props) => {
 
     return (
